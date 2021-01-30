1f67688239d0096ff83686bebe11674724f95622
ZmaxZboss <max.czekaj@gmx.at>
2021-01-30 21:11:33 +0100

    Privater Unity Ordner + Code

---
+++ Project/Unity-J&R_Max/Assets/Scripts/Plattform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plattform : MonoBehaviour
+{
+    private GameObject[] plattform;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plattform = GameObject.FindGameObjectsWithTag("Plattform");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed);
+    }
+}
